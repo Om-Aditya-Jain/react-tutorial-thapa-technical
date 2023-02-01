5294e1b07cc1686f922836d11c4ad45892bcb1cd
Om <adityajain2002j@gmail.com>
2023-02-01 23:58:50 +0530

    learning react

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,10 +3,15 @@ import {Link} from 'react-router-dom';
 const Menu = () =>{
     return (
         <>
-        <Link to="/om">Home</Link>
-        <Link to="/About">About</Link>
-        <Link to="/Contact">Contact</Link>
-        <Link to="/Service">Service</Link>
+        <div style={{color:"white",background:"black",height:"7vh",width:"100%",display:"flex", gap: "10px"}}>
+        <Link to="/Home" style={{color:"white", fontSize:"large", margin:"0x 5px"}}>Home</Link>
+        &emsp;
+        <Link to="/About" style={{color:"white", fontSize:"large", margin:"0x 5px"}}>About</Link>
+        &emsp;
+        <Link to="/Contact" style={{color:"white", fontSize:"large", margin:"0x 5px"}}>Contact</Link>
+        &emsp;
+        <Link to="/Service" style={{color:"white", fontSize:"large", margin:"0x 5px"}}>Service</Link>
+        </div>
         </>
     );
 }
